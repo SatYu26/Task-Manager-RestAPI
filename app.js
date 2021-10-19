c2514802ad35bcaf53f9ae168d0751643930d3b2
SatYu26 <goyalsatyam8@gmail.com>
2021-10-19 19:33:24 +0530

    routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Task Manager App')
+const express = require("express");
+
+const app = express();
+
+const tasks = require("./routes/tasks");
+
+// middlewares
+
+app.use(express.json());
+
+// routes
+
+app.get("/hello", (req, res) => {
+  res.send("Task manager app");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listning on port ${port}`));
---
+++ routes/tasks.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
+
+const router = express.Router();
+
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+
+module.exports = router;
