8ca4fe9ce3c27e3b9acfd4f58f87f7638135360d
SatYu26 <goyalsatyam8@gmail.com>
2021-10-28 18:57:33 +0530

    connected with mongodb database

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middlewares
 
@@ -18,4 +20,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listning on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listning on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
